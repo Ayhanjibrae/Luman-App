926f56d915ecc2ca106bc78a770676b70e17d43b
Ayman <aymanjibrael@icloud.com>
2022-12-30 18:56:07 +0300

    Initial Commit

---
+++ SosyalMedia/SosyalMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SosyalMediaApp.swift
+//  SosyalMedia
+//
+//  Created by Ayhan on 30.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SosyalMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
